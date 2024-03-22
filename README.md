c79ed2fdaf5c4f19027bdc0d99fda75a72d4e03c
TomiG <tomi.groning@solidabis.com>
2024-03-22 12:09:39 +0200

    move examples to esim-folder. Clone example to osa 1/kurssitiedot for the base of excercises

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # helsinki-full-stack-kurssi
-Full-stack kurssin tehtävät
+Full-stack kurssin tehtävät osien mukaan nimetyissä kansioissa. "osa1", "osa2" jne.
+"esim" -kansiossa kurssin esimerkkitehtäviä ja niiden perusteella protoilua.
---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,31 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by 
+      <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [ 'Leevi', 'Venla']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+export default App
