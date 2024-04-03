e994440d13205e620b64bff7a3f490c536584aba
TWODAY-TG\Tomi <tomi.groning@twoday.com>
2024-04-03 15:50:53 +0300

    exercise 2.4

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,9 +1,9 @@
 const Header = (props) => {
   console.log(props.course)
   return (
-    <h1>
+    <h2>
       {props.course}
-    </h1>
+    </h2>
   )
 }
 
@@ -32,7 +32,7 @@ const Total = ({parts}) => {
   const total = parts.reduce( ( total, part) => {
     return total+part.exercises
   },0)
-  
+
   return (
     <b>
       Total of {total} exercises 
@@ -54,38 +54,59 @@ const Course = ({course}) => {
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-      ,
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
 
   return (
+
     <div>
-    <Course key={course.parts.id} course={course} />
+      <h1>Web development curriculum</h1>
+      {courses.map(course =>
+        <Course key={course.id} course={course} />
+      )}
   </div>
   )
 }
