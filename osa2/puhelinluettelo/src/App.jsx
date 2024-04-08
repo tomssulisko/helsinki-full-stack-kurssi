748fd68c9710dc62e32722c0a6d0c4219127007c
TWODAY-TG\Tomi <tomi.groning@twoday.com>
2024-04-08 16:01:06 +0300

    exercise 2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,32 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import PersonsList from './components/PersonsList'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-123456',
-      id:'1' 
-    },
-    { 
-      name: 'Ada Lovelace', 
-      number: '39-44-5323523', 
-      id:'2' },
-    { 
-      name: 'Dan Abramov', 
-      number: '12-43-234345', 
-      id:'3'  },
-    {
-      name: 'Merja Pallas',
-      number: '060-453573436',
-      id:'4' 
-    }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterPhrase, setFilterPhrase] = useState('')
+
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
 
   const addPerson = (event) => {
@@ -41,8 +34,8 @@ const App = () => {
 
     if(!nameIsAlreadyAdded) {
       setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+      setNewName('')
+      setNewNumber('')
     } else {
       alert(newName+" is already added to phonebook")
     }
