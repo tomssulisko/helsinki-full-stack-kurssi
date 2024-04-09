bfdac402df88c498229bffa84c8b7a250e6853e4
TWODAY-TG\Tomi <tomi.groning@twoday.com>
2024-04-09 16:03:56 +0300

    update esim

--- esim/part2/src/components/Note.jsx
+++ esim/part2/src/components/Note.jsx
@@ -1,7 +1,14 @@
-const Note = ({ note }) => {
-    return (
-      <li>{note.content}</li>
-    )
-  }
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important
+    ? 'make not important' : 'make important'
+
+  return (
+    <li>
+      {note.content} 
+      &nbsp;
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  )
+}
   
   export default Note
