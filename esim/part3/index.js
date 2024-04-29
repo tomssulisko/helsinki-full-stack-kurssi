2ae757e5dc61e63da2767d78cad1fb81cdc8bb7b
TWODAY-TG\Tomi <tomi.groning@twoday.com>
2024-04-29 14:55:57 +0300

    create esim part3

---
+++ esim/part3/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+
+
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
+  
+  app.get('/api/notes/:id', (request, response) => {
+    const id = Number( request.params.id)
+    const note = notes.find(note => {
+      console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+      return note.id === id
+    })
+    if (note) {
+      console.log(note)
+    response.json(note)  
+    } else {
+        response.status(404).end()
+    } 
+  })
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
